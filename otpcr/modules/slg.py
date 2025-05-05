5a505ea579c93d25b119d5a497c7e018f1399519
Bart Thate <bthate@dds.nl>
2025-05-05 07:56:30 +0200

    update slogan

--- otpcr/modules/slg.py
+++ otpcr/modules/slg.py
@@ -4,8 +4,7 @@
 "slogan"
 
 
-TXT = "By law using poison, castrated, tortured, killed, destructed in whole or in part since 4 March 2019. @KarimKhanQC reconsider OTP-CR-117/19 http://bit.ly/gcid3 !"
-TXT = "By law using poison, castrated, tortured, killed, destructed in whole/in part since 4 March 2019 @KarimKhanQC reconsider OTP-CR-117/19 http://otpcr.github.io !"
+TXT = "By law using poison, castrated, tortured, killed, destructed in whole/in part since 4 March 2019. @KarimKhanQC reconsider OTP-CR-117/19 http://otpcr.github.io !"
 
 
 def slg(event):
